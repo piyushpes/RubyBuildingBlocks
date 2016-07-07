b703b8fe217702ba979db57360ba51d9ff8f3435
Piyush <piyushpes@gmail.com>
2016-07-08 01:23:29 +0530

    =Caesar Cipher function

--- project1.rb
+++ project1.rb
@@ -1,24 +1,30 @@
-puts "Enter a string"
-
-str=gets.chomp
-str=str.unpack("C*")
-puts "Enter shift factor"
-shif=gets.chomp.to_i
-
-str1=str.map do |var|
+def caesar_cipher str,shif
 	
-	if (97..122)===var
+	str=str.unpack("C*")
+	
+	str1=str.map do |var|
+	
+		if (97..122)===var
 
 		var=(var-97-shif)%26
 		var+=97
 		
-    elsif (65..90)===var
+    	elsif (65..90)===var
+    	
     	var=(var-65-shif)%26
 		var+=65
-    end
+    	
+    	end
 
-var
+	var
+	end
+	puts str1.pack("C*")
 end
 
+puts "Enter a string"
+text=gets.chomp
 
-puts str1.pack("C*")
+puts "Enter shift factor"
+offset=gets.chomp.to_i
+
+caesar_cipher text,offset
